9a30246da8a5032ab67af8a8a8e7caf14607c609
James Kabbes <james.kabbes@gmail.com>
2023-01-18 13:34:01 -0600

    configuring for client upgrades

--- src/real_time_input/Client.py
+++ src/real_time_input/Client.py
@@ -1,17 +1,21 @@
+import kabbes_client
 import real_time_input
-import kabbes_user_client
 import py_starter as ps
 
 class Client( real_time_input.RealTimeInput ):
 
-    BASE_CONFIG_DICT = {
-        "_Dir": real_time_input._Dir,
-        "PLATFORM_SYSTEM": real_time_input.PLATFORM_SYSTEM,
+    _BASE_DICT = {
+        'PLATFORM_SYSTEM': real_time_input.PLATFORM_SYSTEM,
         'KEY_MAPPING': real_time_input.KEY_MAPPING
     }
 
-    def __init__( self, dict={}, **kwargs ):
+    def __init__( self, dict={} ):
 
-        dict = ps.merge_dicts( Client.BASE_CONFIG_DICT, dict )
-        self.cfg_rti = kabbes_user_client.Client( dict=dict, **kwargs ).cfg
+        d = {}
+        d.update( Client._BASE_DICT )
+        d.update( dict )
+
+        self.Package = kabbes_client.Package( real_time_input._Dir, dict=d )
+        self.cfg_rti = self.Package.cfg
+
         real_time_input.RealTimeInput.__init__( self )
--- src/real_time_input/__main__.py
+++ src/real_time_input/__main__.py
@@ -1,6 +1,7 @@
 import real_time_input
 
 RTI = real_time_input.Client()   
+#RTI.cfg_rti.print_atts()
 
 print ('Type to add words to your selection: ')
 RTI.get_multiple_inputs()
